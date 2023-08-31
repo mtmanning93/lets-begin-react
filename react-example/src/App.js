128782f50b52e2f7f4714ce63f66d03435144b15
Matt Manning <mtmanning93@hotmail.co.uk>
2023-08-31 06:19:41 +0000

    Create new component with props

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
+import FunctionalGreetingWithProps
+ from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      <FunctionalGreetingWithProps />
     </div>
   );
 }
---
+++ react-example/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const FunctionalGreetingWithProps = () => <h1>Props</h1>
+
+export default FunctionalGreetingWithProps

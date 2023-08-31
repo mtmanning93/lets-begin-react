4a4cd8fb9857d1f12733f7a6882c489e11a95208
Matt Manning <mtmanning93@hotmail.co.uk>
2023-08-31 08:39:51 +0000

    Create new file for Prev State

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -4,13 +4,15 @@ import FunctionalGreetingWithProps
  from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Matt" age="30" /> */}
       {/* <StatefulGreeting greeting="Im a stateful class component" name="Bob" /> */}
-      <StatefulGreetingWithCallback greeting="Im a stateful class component" name="Bob" />
+      {/* <StatefulGreetingWithCallback greeting="Im a stateful class component" name="Bob" /> */}
+      <StatefulGreetingWithPrevState greeting="Im a stateful class component" name="Bob" />
     </div>
   );
 }
